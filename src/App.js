f42472347be5b37d020a8fd1852039bcaf009031
VitaliyZhark <vitaliy.zhark@gmail.com>
2023-03-22 21:14:32 +0000

    update test
    update test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <p>Demo Text</p>
         <p>Demo Text</p>
-        <a
+        <p>Demo Text</p>
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
