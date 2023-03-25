59c57c56907c3e2addf29c216ec60142116d26bd
Natalia Sergovantseva <corevalues677@gmail.com>
2023-03-25 19:02:43 +0000

    added API quotes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
+ 
+ 
 import React from 'react';
 import Navbar from './NavBar';
 import HeroSection from './HeroSection';
 import ConditionsSection from './ConditionsSection';
+
+import { styled } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const StyledButton = styled(Link)`
+  background-color: #5ce0d8;
+  color: #fff;
+  border-radius: 50%;
+  font-size: 60px;
+  width: 120px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 61%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-decoration: none;
+`;
 
 const Home = () => {
   return (
@@ -9,6 +31,9 @@ const Home = () => {
       <Navbar />
       <HeroSection />
       <ConditionsSection />
+      <StyledButton to="/QuoteGenerator">
+        <span style={{ fontSize: '1.5rem' }}>Get Quote</span>
+      </StyledButton>
     </>
   );
 };
