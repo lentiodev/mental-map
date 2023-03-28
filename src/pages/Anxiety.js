386ad106b244c53314bc7eb1d062276bc8be74b8
Iwona Malkinska <ivonkaa@hotmail.com>
2023-03-28 16:50:44 +0100

    added articles to pages

--- src/pages/Anxiety.js
+++ src/pages/Anxiety.js
@@ -12,9 +12,26 @@ const Anxiety = () => {
     <>
       <Navbar />
       <Container>
-        <Typography variant="h4">ConditionName</Typography>
+        <Typography variant="h4">Anxiety</Typography>
         <Typography>
-          A full description for the ConditionName mental health symptom goes here.
+          <h2>Understanding and Managing Anxiety: Symptoms, Causes, and Treatment Options</h2>
+        Anxiety is a common mental health disorder that affects millions of people around the world. It is a natural response to stress or danger and can be useful in some situations, but when anxiety becomes chronic, it can interfere with daily life and have a negative impact on mental and physical health.
+
+Anxiety disorders are characterized by persistent and excessive worry or fear about everyday situations, events, or activities. These worries can be about specific things, such as social situations or flying, or they can be more general, such as a constant feeling of unease or apprehension.
+
+Symptoms of anxiety can vary from person to person, but they often include physical symptoms such as rapid heartbeat, sweating, trembling, and shortness of breath. Psychological symptoms can also include irritability, difficulty concentrating, restlessness, and sleep disturbances.
+
+
+
+Anxiety can be caused by a variety of factors, including genetics, brain chemistry, and environmental factors. Stressful life events, such as a job loss or the death of a loved one, can also trigger anxiety in some people.
+
+Fortunately, anxiety is a treatable condition, and there are many effective treatment options available. Cognitive-behavioral therapy (CBT) is a common form of therapy for anxiety and involves identifying and changing negative thought patterns and behaviors that contribute to anxiety. Medications such as antidepressants and anti-anxiety drugs can also be helpful in managing symptoms of anxiety.
+
+In addition to professional treatment, there are also many self-care strategies that can help manage anxiety.
+These can include practicing relaxation techniques such as deep breathing and meditation, getting regular exercise, eating a healthy diet, and getting enough sleep.
+
+It is important to seek help if you are experiencing symptoms of anxiety, as untreated anxiety can lead to other mental health problems and have a negative impact on physical health. 
+If you or someone you know is struggling with anxiety, reach out to a mental health professional or a support group for help and guidance. Remember, you are not alone, and there is help available.
         </Typography>
       </Container>
     </>
--- src/pages/Depression.js
+++ src/pages/Depression.js
@@ -12,9 +12,21 @@ const Depression = () => {
     <>
       <Navbar />
       <Container>
-        <Typography variant="h4">ConditionName</Typography>
-        <Typography>
-          A full description for the ConditionName mental health symptom goes here.
+        <Typography variant="h4">Depression</Typography>
+        <Typography> <h2>Understanding Depression: Symptoms, Causes, and Treatment Options</h2>
+        Depression is a common mental health disorder that affects millions of people around the world. It is characterized by persistent feelings of sadness, hopelessness, and a loss of interest in activities that were once enjoyed.
+
+Symptoms of depression can vary from person to person but often include feelings of worthlessness, changes in appetite or sleep patterns, fatigue, and difficulty concentrating. In some cases, depression can also lead to physical symptoms such as headaches, stomach pain, or back pain.
+
+Depression can be caused by a variety of factors, including genetics, brain chemistry, and environmental factors such as stress or trauma. It can also be a side effect of certain medications or medical conditions.
+
+Treatment for depression typically involves a combination of medication and therapy. Antidepressant medications are commonly used to treat depression and work by altering the levels of neurotransmitters in the brain. Therapy, such as cognitive-behavioral therapy (CBT), can also be helpful in managing symptoms of depression by identifying and changing negative thought patterns and behaviors.
+
+Self-care strategies can also be helpful in managing symptoms of depression. These can include getting regular exercise, eating a healthy diet, getting enough sleep, and practicing relaxation techniques such as deep breathing or meditation.
+
+It is important to seek help if you are experiencing symptoms of depression, as untreated depression can lead to other mental health problems and have a negative impact on physical health. If you or someone you know is struggling with depression, reach out to a mental health professional or a support group for help and guidance. Remember, depression is a treatable condition, and there is help available.
+
+In conclusion, depression is a common mental health disorder that can have a significant impact on an individual's life. Treatment typically involves a combination of medication and therapy, as well as self-care strategies to manage symptoms. It is important to seek help if you are experiencing symptoms of depression, as it is a treatable condition, and there is hope for recovery.
         </Typography>
       </Container>
     </>
