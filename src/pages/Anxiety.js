0cc6fc820fc3fe0774a461c27f67302799482ebf
Iwona Malkinska <ivonkaa@hotmail.com>
2023-03-29 09:35:35 +0100

    pages conditions and about page

--- src/pages/Anxiety.js
+++ src/pages/Anxiety.js
@@ -2,23 +2,52 @@ import React from 'react';
 import Navbar from '../components/NavBar';
 import { Typography } from '@mui/material';
 import { styled } from '@mui/material';
+import anxietyImage from '../images/anxiety.png'
 
 const Container = styled('div')`
   padding: 40px;
 `;
 
-const Anxiety = () => {
+const TextWrapper = styled('div')`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const ImageWrapper = styled('div')`
+  flex-basis: 400px;
+`;
+
+const anxietyPage = () => {
   return (
     <>
       <Navbar />
       <Container>
-        <Typography variant="h4">ConditionName</Typography>
+        <Typography variant="h4" sx={{ textTransform: 'uppercase' }}>ANXIETY</Typography>
         <Typography>
-          A full description for the ConditionName mental health symptom goes here.
+          <p>By Jane Doe | Published on March 27, 2023</p>
+          <h2>Understanding and Managing Anxiety: Symptoms, Causes, and Treatment Options</h2>
+          <TextWrapper>
+            <ImageWrapper>
+              <img src={anxietyImage} alt="anxiety" style={{ maxWidth: '100%' }} />
+            </ImageWrapper>
+            <div>
+              <Typography>
+                <p>Anxiety is a common mental health disorder that affects millions of people around the world. It is a natural response to stress or danger and can be useful in some situations, but when anxiety becomes chronic, it can interfere with daily life and have a negative impact on mental and physical health.</p>
+                <p>Anxiety disorders are characterized by persistent and excessive worry or fear about everyday situations, events, or activities. These worries can be about specific things, such as social situations or flying, or they can be more general, such as a constant feeling of unease or apprehension.</p>
+                <p>Symptoms of anxiety can vary from person to person, but they often include physical symptoms such as rapid heartbeat, sweating, trembling, and shortness of breath. Psychological symptoms can also include irritability, difficulty concentrating, restlessness, and sleep disturbances.</p>
+                <p>Anxiety can be caused by a variety of factors, including genetics, brain chemistry, and environmental factors. Stressful life events, such as a job loss or the death of a loved one, can also trigger anxiety in some people.</p>
+                <p>Fortunately, anxiety is a treatable condition, and there are many effective treatment options available. Cognitive-behavioral therapy (CBT) is a common form of therapy for anxiety and involves identifying and changing negative thought patterns and behaviors that contribute to anxiety. Medications such as antidepressants and anti-anxiety drugs can also be helpful in managing symptoms of anxiety.</p>
+                <p>In addition to professional treatment, there are also many self-care strategies that can help manage anxiety. These can include practicing relaxation techniques such as deep breathing and meditation, getting regular exercise, eating a healthy diet, and getting enough sleep.</p>
+                <p>It is important to seek help if you are experiencing symptoms of anxiety, as untreated anxiety can lead to other mental health problems and have a negative impact on physical health. If you or someone you know is struggling with anxiety, reach out to a mental health professional or a support group for help and guidance. Remember, you are not alone, and there is help available.</p>
+              </Typography>
+            </div>
+          </TextWrapper>
         </Typography>
       </Container>
     </>
   );
 };
 
-export default Anxiety;
+export default anxietyPage;
+
--- src/pages/Autism.js
+++ src/pages/Autism.js
@@ -1,7 +1,11 @@
+
+
+
 import React from 'react';
 import Navbar from '../components/NavBar';
 import { Typography } from '@mui/material';
 import { styled } from '@mui/material';
+import autismImage from '../images/autism.png'
 
 const Container = styled('div')`
   padding: 40px;
@@ -12,9 +16,29 @@ const Autism = () => {
     <>
       <Navbar />
       <Container>
-        <Typography variant="h4">ConditionName</Typography>
-        <Typography>
-          A full description for the ConditionName mental health symptom goes here.
+      <Typography variant="h4" sx={{ textTransform: 'uppercase' }}>AUTISM</Typography>
+      <p>By Leonard Atkinson | Published on March 27, 2023</p>
+        <Typography> <h2>Understanding Autism Spectrum Disorder: Symptoms, Causes, and Treatment Options</h2>
+        <img src={autismImage} alt="autism" style={{ maxWidth: '100%', width: '400px', float: 'left', marginRight: '20px' }} />
+
+    <p>Autism, or Autism Spectrum Disorder (ASD), is a neurodevelopmental disorder that affects communication, social interaction, and behavior. The disorder is typically diagnosed in early childhood, and its prevalence has increased in recent years, with an estimated 1 in 54 children in the United States being diagnosed with ASD.</p>
+
+    <p>The symptoms of autism can vary widely, but common signs include delayed speech and language development, difficulty with social interactions, repetitive behaviors or routines, and heightened sensitivity to certain sensory inputs. Some individuals with autism may also have other health issues, such as seizures, anxiety, or gastrointestinal problems.</p>
+
+<p>The causes of autism are not entirely understood, but research suggests that a combination of genetic and environmental factors play a role. Studies have identified several genes associated with autism, and research has also shown that exposure to certain toxins or infections during pregnancy may increase the risk of developing the disorder.</p>
+
+<p>There is currently no cure for autism, but early intervention can be helpful in improving outcomes for individuals with the disorder. Treatment typically involves a combination of behavioral, educational, and medical interventions, tailored to the specific needs of each individual.</p>
+
+<p>Behavioral interventions, such as Applied Behavior Analysis (ABA) therapy, can help individuals with autism learn new skills and behaviors and reduce problematic behaviors. Educational interventions, such as speech and language therapy, can help improve communication skills, while occupational therapy can help individuals with autism develop daily living skills and improve their motor skills.</p>
+
+<p>Medications may also be used to manage symptoms associated with autism, such as anxiety or hyperactivity, but it is important to work closely with a healthcare provider when using medication for any mental health disorder.</p>
+
+<p>It is important to note that individuals with autism have unique strengths and abilities and should be supported in their individuality. Many individuals with autism possess exceptional skills in areas such as music, art, and math, and may excel in certain professions.</p>
+
+<p>There is also a growing movement to promote neurodiversity and celebrate the unique perspectives and strengths of individuals with autism and other neurological differences.</p>
+
+<p>In conclusion, autism is a complex disorder that can have a significant impact on an individual's life. Early intervention and a tailored treatment plan can be helpful in improving outcomes for individuals with autism, and it is important to support and celebrate the unique strengths and perspectives of individuals with autism and other neurological differences.</p>
+
         </Typography>
       </Container>
     </>
